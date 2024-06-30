e8043bfd0c962a34031d360a009d11e2a0b90c98
anindyadiva06 <134478029+anindyadiva06@users.noreply.github.com>
2024-06-30 22:45:12 +0700

    Update ArtikelController.php

--- app/Http/Controllers/ArtikelController.php
+++ app/Http/Controllers/ArtikelController.php
@@ -1,21 +1,74 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Admin;
 
 use App\Models\Artikel;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
+use App\Http\Controllers\Controller;
+
 
 class ArtikelController extends Controller
 {
-    public function home(): View
+    public function index() : View
     {
         $artikel_signwave = Artikel::all();
-        return view('home-user', compact('artikel_signwave'));
+        return view('admin.artikel.index', compact('artikel_signwave'));
     }
-    public function index(): View
+    
+    public function tambah() : View
     {
-        $artikel_signwave = Artikel::all();
-        return view('index', compact('artikel_signwave'));
+        return view('admin.artikel.tambah');
+    }
+
+    public function store(Request $request)
+    {
+    $validatedData = $request->validate([
+        'judul_artikel' => 'required|max:255',
+        'konten' => 'required',
+        'link' => 'required|url',
+    ]);
+
+    $artikel_signwave = new Artikel;
+    $artikel_signwave->judul_artikel = $request->judul_artikel;
+    $artikel_signwave->konten = $request->konten;
+    $artikel_signwave->link = $request->link;
+    $artikel_signwave->save();
+
+    return redirect()->back()->with('success', 'Artikel berhasil ditambahkan!');
+    }
+
+    public function edit($id) : View
+    {
+        $artikel_signwave = Artikel::findOrFail($id);
+        return view('admin.artikel.edit', compact('artikel_signwave'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'judul_artikel' => 'required',
+            'konten' => 'required',
+            'link' => 'required|url',
+        ]);
+
+       
+        $artikel_signwave = Artikel::find($id);
+        $artikel_signwave->judul_artikel = $request->judul_artikel;
+        $artikel_signwave->konten = $request->konten;
+        $artikel_signwave->link = $request->link;
+        $artikel_signwave->save();
+
+        return redirect()->back()->with('success', 'Artikel berhasil diubah!');
+    }
+
+    public function destroy($id)
+    {
+        $artikel_signwave = Artikel::findOrFail($id);
+        $artikel_signwave->delete();
+
+        return redirect()->back()->with('success', 'Artikel berhasil dihapus!');
     }
 }
+
+?>
