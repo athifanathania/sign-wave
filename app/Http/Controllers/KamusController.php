1aede08632cd9a031dd1b86208d68a6a4e593549
anindyadiva06 <134478029+anindyadiva06@users.noreply.github.com>
2024-07-01 04:55:50 +0700

    Update KamusController.php

--- app/Http/Controllers/KamusController.php
+++ app/Http/Controllers/KamusController.php
@@ -22,4 +22,84 @@ class KamusController extends Controller
     }
 }
 
+//kamus admin
+ public function index() : View
+    {
+        $kamus_signwave = Kamus::simplePaginate(20);
+        return view('admin.kamus.index', compact('kamus_signwave'));
+    }
+
+    public function tambah() : View
+    {
+        return view('admin.kamus.tambah');
+    }
+
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'kata' => 'required',
+            'deskripsi' => 'required',
+            'gambar' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048',
+        ]);
+    
+        $gambarPath = $request->file('gambar')->storeAs('assets/img/kamus', $request->file('gambar')->getClientOriginalName(), 'public');
+    
+        $kamus_signwave = new Kamus;
+        $kamus_signwave->kata = $request->kata;
+        $kamus_signwave->deskripsi = $request->deskripsi;
+        $kamus_signwave->gambar = $gambarPath;
+        $kamus_signwave->save();
+    
+        return redirect()->back()->with('success', 'Kamus berhasil ditambahkan!');
+    }
+
+    public function edit($id) : View
+    {
+        $kamus_signwave = Kamus::findOrFail($id);
+        return view('admin.kamus.edit', compact('kamus_signwave'));
+    }
+
+    public function update(Request $request, $id)
+    {
+    $validatedData = $request->validate([
+        'kata' => 'required',
+        'deskripsi' => 'required',
+        'gambar' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048',
+    ]);
+
+    $kamus_signwave = Kamus::findOrFail($id);
+    $kamus_signwave->kata = $request->kata;
+    $kamus_signwave->deskripsi = $request->deskripsi;
+
+    if ($request->hasFile('gambar')) {
+        if ($kamus_signwave->gambar) {
+            \File::delete(public_path('assets/img/kamus/' . $kamus_signwave->gambar));
+        }
+    
+        $gambarPath = $request->file('gambar')->store('assets/img/kamus', 'public');
+        $kamus_signwave->gambar = basename($gambarPath); 
+    }
+
+    $kamus_signwave->save();
+
+    return redirect()->back()->with('success', 'Kamus berhasil diubah!');
+    }
+
+    public function destroy($id)
+    {
+        $kamus_signwave = Kamus::findOrFail($id);
+
+        if ($kamus_signwave->gambar) {
+            \File::delete(public_path($kamus_signwave->gambar));
+        }
+
+        $kamus_signwave->delete();
+
+        return redirect()->back()->with('success', 'Kamus berhasil dihapus!');
+    }
+}
+?>
+
+
+
 ?>
